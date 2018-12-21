99e0c0acbee8b28bb967f9f12fb14f790cf5460a
rishabh3 <rishabh97dutt@gmail.com>
2018-12-21 20:40:26 +0530

    Second Commit

--- src/main/java/com/rishabh/core/App.java
+++ src/main/java/com/rishabh/core/App.java
@@ -15,18 +15,6 @@ public class App
     public static long COLLECTION_SIZE = 99999;
     public static int RECORD_SIZE = 37;
     public static long READ_IN_UNITS = 1024;
-
-    public static List<Long> getSkips(long start, long end, long skip) {
-        List<Long> mylist = new ArrayList<Long>();
-        for( long i = start; i < end;i+=skip ) {
-            mylist.add(i);
-        }
-        if  (mylist.get(mylist.size()-1) < COLLECTION_SIZE) {
-            mylist.add(COLLECTION_SIZE);
-        }
-        return mylist;
-    }
-
     public static void main( String[] args )
     {
 
@@ -42,25 +30,15 @@ public class App
             batch_size = COLLECTION_SIZE;
         }
 
-        List<Long> skips = getSkips(0, COLLECTION_SIZE, batch_size);
-
-        List<Task> thread_work = distributeWork(skips, client_thread, batch_size);
-
-        // for (Task var : thread_work) {
-        //     System.out.println(var);   
-        // }
-
         List<ClientThread> myclients = new ArrayList<ClientThread>();
 
-        List<Double> throughput = new ArrayList<Double>();
         ClientThread tempClient;
-        int client_num = 0;
+    
         long start = System.currentTimeMillis();
-        for (Task var : thread_work) {
-            tempClient = new ClientThread(batch_size, client_num, throughput);
+        for (int i = 0; i < client_thread; i++) {
+            tempClient = new ClientThread(batch_size, i);
             myclients.add(tempClient);
-            tempClient.start(var);
-            client_num++;
+            tempClient.start();
         }
         for (ClientThread var : myclients) {
             try {
@@ -73,27 +51,21 @@ public class App
         long end = System.currentTimeMillis();
         long dur = end - start;
         // System.out.println("DEBUG: " +String.valueOf(dur) +"ms");
-        double thr = calculateThr(dur);
+        double thr = calculateThr(dur, batch_size ,client_thread);
         // System.out.println("Throughput A: " + String.valueOf(thr) + " KB/s");
-        double sum = 0.0;
-        for (Double var : throughput) {
-            sum += var;
-        }
-        double thrA = sum / (double) client_num;
         // System.out.println("Throughput B: " + String.valueOf(thrA) + " KB/s");
         
-        writeToFile(args[0], client_num, batch_size, thr, thrA);
+        writeToFile(args[0], client_thread, batch_size, thr);
         sc.close();
     }
 
-    private static void writeToFile(String path, int client_num, long batch_size, double thrA, double thrB) {
+    private static void writeToFile(String path, int client_num, long batch_size, double thrA) {
         FileWriter fileWriter = null;
         File file;
         try{
             file = new File(path);
             fileWriter = new FileWriter(file, true);
-            String msg = String.valueOf(client_num) + "\t" + String.valueOf(batch_size) + "\t" + String.valueOf(thrA) + "\t" + String.valueOf(thrB)+
-            "\n";
+            String msg = String.valueOf(client_num) + "," + String.valueOf(batch_size) + "," + String.valueOf(thrA) + "\n";
             fileWriter.write(msg);
             fileWriter.close();
         }
@@ -101,33 +73,14 @@ public class App
             e.printStackTrace();
         }
     }
-    private static double calculateThr(long dur) {
-        double res = (double) COLLECTION_SIZE / (double) READ_IN_UNITS;
+    private static double calculateThr(long dur, long batch_size, int client_num) {
+        double res = (double) batch_size / (double) READ_IN_UNITS;
         // System.out.println("DEBUG: " + String.valueOf(res));
+        res *= (double)client_num;
         res *= (double)((double)RECORD_SIZE / (double)dur);
         // System.out.println("DEBUG: " + String.valueOf(res));
         res *= (double)1000;
         // System.out.println("DEBUG: " + String.valueOf(res));
         return res;
-    }
-
-    private static List<Task> distributeWork(List<Long> skips, int thread_count, long batch_size) {
-        List<Task> tasks = new ArrayList<Task>();
-        int start = 0, end = 0;
-        int num_groups = (int) (skips.size() / thread_count);
-        Task tempTask;
-        for(int i = 0;i < thread_count;i++ ){
-            if (i == thread_count - 1) {
-                tempTask = new Task(skips, end, skips.size()-1, i, batch_size);
-                tasks.add(tempTask);
-            }
-            else {
-                start = i* num_groups;
-                end = (i+1) * num_groups;
-                tempTask = new Task(skips, start, end, i, batch_size);
-                tasks.add(tempTask);
-            }
-        }
-        return tasks;
     }
 }
--- src/main/java/com/rishabh/core/ClientThread.java
+++ src/main/java/com/rishabh/core/ClientThread.java
@@ -1,11 +1,8 @@
 package com.rishabh.core;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-import com.mongodb.DBCursor;
+import java.util.concurrent.ThreadLocalRandom;
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
@@ -18,51 +15,21 @@ public class ClientThread extends Thread {
     private MongoClient mongoConn;
     private MongoDatabase database;
     private MongoCollection<Document> collection;
-    private List<Integer> dataRead;
-    private Task work;
-    private List<Double> throughputDoubles;
-    private final Lock listLock = new ReentrantLock();
 
     private int threadNumber;
     private long batch;
 
-    public ClientThread(long batch, int threadNumber, List<Double> globalThroughpDoubles) {
+    public ClientThread(long batch, int threadNumber) {
         this.batch = batch;
         this.threadNumber = threadNumber;
-        this.dataRead = new ArrayList<Integer>();
-        this.throughputDoubles = globalThroughpDoubles;
     }
 
     public void run() {
-        this.mongoConn = new MongoClient("localhost", 27017);
+        this.mongoConn = new MongoClient("10.10.1.202", 27017);
         this.database = this.mongoConn.getDatabase("test");
         this.collection = this.database.getCollection("mycol");
-        long next_task;
-        
-        long start = System.currentTimeMillis();
-        while(work.getStatus() == false) {
-            next_task = work.getNextElement();
-            if(next_task == -1) {
-                continue;
-            }
-            this.process_cursor(next_task);
-        }
-        long end = System.currentTimeMillis();
-        long duration = end - start;
-        int recordRead = this.sumup();
-        // System.out.println("Thread " + String.valueOf(this.threadNumber) + " read " + String.valueOf(recordRead));
-        
-        // Calculate throughput
-        double thr = this.calculateThroughput(recordRead, duration);
-        // System.out.println("Throughput: " + String.valueOf(thr) + " KB/s");
-        // Put the throughput in global structure (Locking)
-        listLock.lock();
-        try{
-            this.throughputDoubles.add(thr);
-        }finally{
-            listLock.unlock();
-        }
-
+        long rand = this.getRandomNumber();
+        this.process_cursor(rand);
         // System.out.println("Connection opened to MongoD instance running at localhost:27017");
         this.cleanup();
     }
@@ -73,36 +40,18 @@ public class ClientThread extends Thread {
         int batch_size = (int) clientThread.batch;
         int skip_ind = (int) skip;
         FindIterable<Document> findIterable = clientThread.collection.find().skip(skip_ind).limit(batch_size);
-        List<Document> data = new ArrayList<Document>();
-        for (Document var : findIterable) {
-            data.add(var);
-        }
-        this.dataRead.add(data.size());
     }
     
-    public void start(Task task) {
-        // System.out.println("This is start method");
-        this.work = task;
-        super.start();
+    private long getRandomNumber() { 
+        long diff = App.COLLECTION_SIZE - this.batch;
+        return ThreadLocalRandom.current().nextLong(0, diff);
     }
-    
+
     public void cleanup() {
         // System.out.println("Going to clean up the states");
         this.mongoConn.close();
         this.mongoConn = null;
         this.database = null;
         this.collection = null;
-    }
-
-    private double calculateThroughput(int numRecordsRead, long duration_overall) {
-        return (numRecordsRead * App.RECORD_SIZE * 1000) / (App.READ_IN_UNITS * duration_overall);  
-    }
-
-    private int sumup() {
-        int sum = 0;
-        for (Integer var : this.dataRead) {
-            sum += var;
-        }
-        return sum;
     }
 }
